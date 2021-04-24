fee84260443881c6387bb82786c5a56eca7a010b
Vladimir Kamuz <v.kamuz@gmail.com>
2021-04-24 12:36:02 +0300

    Added Betting Nirvana and Top Canadian Casino Games projects

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,7 +28,7 @@ function removeClass(element, name) {
   arr2 = name.split(" ");
   for (i = 0; i < arr2.length; i++) {
     while (arr1.indexOf(arr2[i]) > -1) {
-      arr1.splice(arr1.indexOf(arr2[i]), 1);     
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
     }
   }
   element.className = arr1.join(" ");
